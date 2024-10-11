297571ac1ba394fb00082d7b69a15fb630c43864
BaekJiWook <ooooii1@hanmail.net>
2024-10-11 16:20:33 +0900

    10/11

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -1,54 +1,31 @@
 <template>
-  <div class="event" :class="{show:isOpen}">
-  <!-- class="event show클래스를 두개 설정 시 그냥 두개 적으면 된다 
-     하지만 상태를 변화시키기 위해 v-bind로 설정할것임-->
-  <!-- :class="{show:true}" show라는 클래스는 true일때만 활성화-->
-  <!-- 상태변수 값으로 추가할거야(isOpen)-->
+  <div class="event">
     <p>{{ text }}</p>
-    <button @click="isOpen=false">X</button>
   </div>
 </template>
-  
-<script>
-  export default {
-    name: "EventComponent",
-    props: {
-      text: String,
-    },
-    data(){
-      return{
-        isOpen: true,
-        // 상태변수 값으로 추가할거야(isOpen)
-      }
-    }
+
+<script setup>
+import { defineProps } from 'vue'
+
+// props 정의
+const props = defineProps({
+  text: {
+    type: String,
+    required: true,
   }
+})
 </script>
-  
+
 <style>
-  .event {
+.event {
   background: #444;
   color: #fff;
   padding: 10px 20px;
   text-align: center;
   font-size: small;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  display: none;
-  /* 이벤트창이 기본값으로 보이지 않게 설정
-  _클릭이벤트로 보이게 설정하게 위해 */
-}
-
-.show {
-  display: flex;
-  /* show라는 클래스는 기본적으로 보인다(flex)는 뜻임 */
 }
 
 .event p {
-  margin: 0;
-}
-
-.event button {
   margin: 0;
 }
 </style>
